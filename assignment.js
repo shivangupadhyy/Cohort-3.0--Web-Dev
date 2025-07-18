57cc5405882937142628ae028de57b07cc9f86aa
Shivang Upadhyay <shivang14071993@gmail.com>
2025-07-18 12:12:33 +0530

    Add initial HTML, CSS, and JavaScript files for Week 1 website setup

---
+++ assignment.js
@@ -0,0 +1,19 @@
+let a, b;
+function sum(a, b) {
+    const c = a + b;
+    console.log(c)
+}
+sum(9, 1);
+
+
+let age = 19
+function canVote() {
+    if (age > 18) {
+        return true
+    }
+    else {
+        return true
+    }
+}
+let ans  = canVote();
+console.log(ans)
