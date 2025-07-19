3fca440c048c7562d11c44950f9c7f47799ab672
Shivang Upadhyay <shivang14071993@gmail.com>
2025-07-19 10:34:42 +0530

    completed a week 1 of cohort 3

--- assignment.js
+++ assignment.js
@@ -1,19 +1,91 @@
-let a, b;
-function sum(a, b) {
-    const c = a + b;
-    console.log(c)
-}
-sum(9, 1);
+// let a, b;
+// function sum(a, b) {
+//     const c = a + b;
+//     console.log(c)
+// }
+// sum(9, 1);
 
 
-let age = 19
-function canVote() {
-    if (age > 18) {
-        return true
+// let age = 19
+// function canVote() {
+//     if (age > 18) {
+//         return true
+//     }
+//     else {
+//         return true
+//     }
+// }
+// let ans  = canVote();
+// console.log(ans)
+
+// let users = ["shivang", "Ayushi", "prabhat", "ansh"];
+
+// for(let i = 0; i<4; i++){
+//     console.log(users[i])
+// }
+
+// function greet(user){
+//     console.log(`hi ${user.name} your age is ${user.age}`)
+// }
+
+// let user = {
+//     name: "Shivang",
+//     age: 19,
+// }
+
+// greet(user);
+
+//  let arr = ["harkirat", 21, {
+//     name : "harkirat",
+//     age: 21,
+//     citites: ["delhi", "chandigarh", "bangalore",{
+//         country: "malaysia",
+//         city: "kuala lumpur"
+//     }]
+//  }];
+
+//  console.log(arr[2].citites[3].city)
+
+//create a function that takes an array of object as input, and returns the user whose age > 18 and are male
+
+// function getAdultMales(arr){
+//     //initialise a new array, push to  new array
+
+//     let arr2 = [];
+//     for(let i = 0; i<arr.length; i++){
+//         if(arr[i].age > 18 && arr[i].gender === "male"){
+//             arr2.push(arr[i]);
+//         }
+//     }
+//     return arr2;
+//     //you can use the filter fucntion inside an array, map, reduce
+// }
+
+function solve(arr){
+    let arr2 = [];
+    for(let i =0; i>18; i++){
+        if(arr[i].age >18 && arr[i].gender === "male"){
+            arr2.push(arr[i])
+        }
     }
-    else {
-        return true
-    }
+    return arr2;
 }
-let ans  = canVote();
-console.log(ans)
+
+
+
+
+const user = [{
+    name: "harkirat",
+    age: 21,
+    gender : "male"
+},{
+    name : "priya",
+    age: 21,
+    gender: "female"
+},{
+    name: "ramen",
+    age: 20,
+    gender: "male"
+}]
+
+console.log(getAdultMales(user));
