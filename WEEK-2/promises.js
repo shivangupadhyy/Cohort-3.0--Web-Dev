d2c20737260da0301e97e7d58aeb16a01420f3b6
Shivang Upadhyay <shivang14071993@gmail.com>
2025-07-20 23:44:17 +0530

    learn about map from tommorow will start refularly and maintain consistancany

--- WEEK-2/promises.js
+++ WEEK-2/promises.js
@@ -1,20 +1,28 @@
-class Rectangle{
-    constructor(widht, height, color){
-        this.width = widht;
-        this.height = height;
-        this.color = color;
-    }
-    area(){
-        const area = this.width * this.height;
-        return area
-        }
+// class Rectangle{
+//     constructor(widht, height, color){
+//         this.width = widht;
+//         this.height = height;
+//         this.color = color;
+//     }
+//     area(){
+//         const area = this.width * this.height;
+//         return area
+//         }
 
-        paint(){
-            console.log(`painting with color ${this.color}`)
-        }
-}
+//         paint(){
+//             console.log(`painting with color ${this.color}`)
+//         }
+// }
 
-const react = new Rectangle(2,4, "red")
-const area = react.area();
-react.paint();
-console.log(area)
+// const react = new Rectangle(2,4, "red")
+// const area = react.area();
+// react.paint();
+// console.log(area)
+
+// const date = new Date();
+// console.log(date.toISOString())
+
+const map = new Map();
+map.set('name', "Shivang")
+map.set('name', "Ayushi")
+console.log(map.get('name'))
