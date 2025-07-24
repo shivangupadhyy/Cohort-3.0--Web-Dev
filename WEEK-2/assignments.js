0094a93a3a246ead6553af0e228ca619c96ccdfd
Shivang Upadhyay <shivang14071993@gmail.com>
2025-07-24 16:25:39 +0530

    changes

---
+++ WEEK-2/assignments.js
@@ -0,0 +1,27 @@
+// function isAnagram(str1, str2) {
+//     const arr1 = str1.toLowerCase().split("");
+//     arr1.sort();
+//     const sortedStr1 = arr1.join("");
+
+//     const arr2 = str2.toLowerCase().split("");
+//     arr2.sort();
+//     const sortedStr2 = arr2.join("");
+
+//     return sortedStr1 === sortedStr2;
+// }
+
+// module.exports = isAnagram;
+
+function findLargestElement(numbers){
+    let max = numbers[0]
+
+    for(let i=1; i<numbers.length; i++){
+        if(numbers[i]>max)
+            max = numbers[i];
+        
+    }
+    // console.log(max)
+    return max
+}
+
+console.log(findLargestElement([5, 9, 10, 8]));
