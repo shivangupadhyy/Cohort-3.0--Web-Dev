30bd35ae8a868f2ca55581fd74021500a09e0a5f
Shivang Upadhyay <shivang14071993@gmail.com>
2025-07-31 23:53:11 +0530

    Add todo list functionality and CLI for file tasks

---
+++ WEEK-4/index.mjs
@@ -0,0 +1,6 @@
+// let firstName = "Shivang Upadhyay";
+// console.log(firstName);
+import chalk from "chalk";;
+console.log(chalk.blue("Hello world"))
+console.log(chalk.red.bold("This is an Error message"));
+console.log(chalk.green.underline("This is a success message"))
