8fd3eb195f55439f1c9951f19b7de35078567154
Shivang Upadhyay <shivang14071993@gmail.com>
2025-08-11 23:27:06 +0530

    Week-5 Upadate

--- Week-5/lecture/index.js
+++ Week-5/lecture/index.js
@@ -1,8 +1,10 @@
 const express  = require("express");
+const cors = require("cors")
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 
 app.post('/sum', (req, res)=>{
