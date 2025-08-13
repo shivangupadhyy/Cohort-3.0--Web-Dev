7e7c3a0bcb191d7d414969a49a855a96b38eeddf
Shivang Upadhyay <shivang14071993@gmail.com>
2025-08-13 17:47:31 +0530

    Week-5 Offline  Upadate

---
+++ Week-05-Offline/Map_FilterAndArrowFns/arrowFunction.js
@@ -0,0 +1,36 @@
+// normal function 
+
+// function greet(){
+//     console.log("hello world")
+// }
+// greet()
+
+//Arrow function to print Hello World - Shorter version
+// const greet = () => console.log('hello world')
+// greet()
+
+//  Normal function to greet a person
+
+// function greetName(name){
+//     console.log(`My name is ${name}`)
+// }
+
+// greetName("shivang");
+
+// Arrow functions to greet a person
+const greetPerson=(name)=>{
+    console.log(`Hello ${name}`)
+}
+
+greetPerson("Shivang")
+
+// const greetName =(name)=>console.log(`hello ${name}`)
+// greetName("Ayushi")
+
+// const sum = (a,b) =>{
+//     return a+b;
+// }
+// console.log(sum(2,6))
+
+const add = (a,b) =>  a+b;
+console.log(add(6,7))
