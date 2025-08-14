9dff43407e307a645d53324a050d8ce92638c656
Shivang Upadhyay <shivang14071993@gmail.com>
2025-08-15 01:12:54 +0530

    Week-6 Upadate

--- Week-06/HTTP-Deep-Dive/Lecture-Codes/01_Basic-Auth/index.js
+++ Week-06/HTTP-Deep-Dive/Lecture-Codes/01_Basic-Auth/index.js
@@ -1,13 +1,26 @@
+// Import the express library 
 const express = require('express');
-const jwt = require("jsonwebtoken");
-const JWT_SECRET = "randomshivayu"
+
+// Create an instance of express application
 const app = express();
 
+// Use the express.json() middleware to parse the request body 
 app.use(express.json());
 
- const user = [];
+// Create an array to store the users username and password
+const users = [];
 
- // Create a function to generate a token for the user
+/*
+[
+    {
+        username: "Bharat",
+        password: "Bharat@123",
+        token: "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6" 
+    }
+]
+*/
+
+// Create a function to generate a token for the user
 function generateToken() {
 
     // Create an array of options for the token 
@@ -27,73 +40,100 @@ function generateToken() {
     return token;
 }
 
-app.post('/signup',(req, res)=>{
+// Create a post request for the signup route
+app.post("/signup", function(req, res) {
+
+    // Get the username and password from the request body
     const username = req.body.username;
     const password = req.body.password;
 
-    user.push({
-        username : username,
-        password : password
-    })
+    // Check if the user is already signed up or not
+    if (users.find(user => user.username === username)) {
+        // Send a response to the client that the user is already signed up
+        return res.json({
+            message: "You are already signed up!"
+        });
+    } 
 
+    // Check if the username has at least 5 characters or not
+    if (username.length < 5) {
+        // Send a response to the client that the username should have at least 5 characters
+        return res.json({
+            message: "You need to have at least 5 users to sign up"
+        });
+    } 
+
+    // Push the username and password to the users array
+    users.push({
+        username: username,
+        password: password
+    });
+
+    // Send a response to the client that the user has signed up successfully
     res.json({
-        message : "You are signed in"
-    })
+        message: "You have signed up successfully!"
+    });
+});
 
-    console.log(user)
-})
-
-app.post('/signin', (req, res)=>{
+// Create a post request for the signin route
+app.post("/signin", function(req, res) {
+    // Get the username and password from the request body
     const username = req.body.username;
     const password = req.body.password;
 
-    const foundUser = user.find(user => user.username === username && user.password === password)
+    // Find the user in the users array with the given username and password 
+    const foundUser = users.find(user => user.username === username && user.password === password);
 
-    if(foundUser){
-        const token = jwt.sign({
-            username: username
-        }, JWT_SECRET)// convert therir unsername over to a jwt
-        // foundUser.token = token;
-        res.json({
-            token : token,
-            message : "You have signed in succesfully!"
-        })
-    }else{
-        res.status(403).send({
-            message: "Invalid Username or Password"
-        })
+    // Check if the user is found or not
+    if (foundUser) {
+        // Generate a token for the user
+        const token = generateToken();
+
+        // Add the token to the user object
+        foundUser.token = token;
+
+        // Send a response to the client with the token
+        return res.json({
+            token: token,
+            message: "You have signed in successfully!",
+        });
+    } else {
+        // Send a response to the client that the user is not found
+        return res.json({
+            message: "Invalid username or password!"
+        });
     }
-    console.log(user)
-})
+});
 
-app.get('/me', (req, res)=>{
-    //get the token from the request headers
-    const token  = req.headers.token;
-    const decodeInfo = jwt.verify(token, JWT_SECRET);//
-    // check if the token is present or not
-    const username = decodeInfo.username;
+// Create a get request for the me route
+app.get("/me", function(req, res) {
+    // Get the token from the request headers
+    const token = req.headers.token;
 
-    if(!token){
-       return res.json({
-            message: "Token not found!"
+    // Check if the token is present or not
+    if (!token) {
+        return res.json({
+            message: "Token is missing!"
         });
     }
 
+    // Find the user in the users array with the given token
+    const foundUser = users.find(user => user.token === token);
 
-    const foundUser = user.find(user =>user.username === username);
-
-    if(foundUser){
-        res.json({
-            username : foundUser.username,
-            password : foundUser.password
+    // Check if the user is found or not 
+    if (foundUser) {
+        // Send a response to the client with the username and password of the user
+        return res.json({
+            username: foundUser.username,
+            password: foundUser.password
         });
-    }else{
-        //send a response to the client that the token is invalid
+    } else {
+        // Send a response to the client that the token is invalid
         return res.json({
             message: "Invalid token!"
-        })
+        });
     }
-})
+});
 
-
-app.listen(3000);// that the ensure http server is listening on the port 3000
+// Start the server on port 3000 
+app.listen(3000);
