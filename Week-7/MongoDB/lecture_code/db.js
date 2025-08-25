3b978bf9496ce22e3f8a6b6d990f1c4f754a5a89
Shivang Upadhyay <shivang14071993@gmail.com>
2025-08-25 11:09:39 +0530

    Week-7 Update

--- Week-7/MongoDB/lecture_code/db.js
+++ Week-7/MongoDB/lecture_code/db.js
@@ -1,23 +1,23 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.ObjectId;
 
 const User = new Schema({
     email : String,
     password: String,
-    name: String
+    name : String
 })
 
 const Todo = new Schema({
     title: String,
-    done: Boolean,
+    done:Boolean,
     userId: ObjectId
 })
 
-const UserModel = mongoose.model('users', User)
+const UserModel = mongoose.model('user', User)
 const TodoModel = mongoose.model('todos', Todo)
 
 module.exports = {
     UserModel: UserModel,
-    TodoModel: TodoModel
+    TodoModel:TodoModel
 }
