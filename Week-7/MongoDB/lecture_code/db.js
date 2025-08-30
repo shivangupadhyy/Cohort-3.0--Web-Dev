7ee28e62e04091b632cd47c86214f057e115781a
Shivang Upadhyay <shivang14071993@gmail.com>
2025-08-30 23:41:42 +0530

    Week-7 Update

--- Week-7/MongoDB/lecture_code/db.js
+++ Week-7/MongoDB/lecture_code/db.js
@@ -1,23 +1,33 @@
+// db.js
+
 const mongoose = require('mongoose');
+
+// Create a new Mongoose Schema instance
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.ObjectId;
 
+// Define the User schema
 const User = new Schema({
-    email : String,
-    password: String,
-    name : String
+    email: {type: String, unique: true}, // email must be unique
+    password: String, // hashed password
+    name: String, // user's name
 })
 
+// Define the Todo schema
 const Todo = new Schema({
-    title: String,
-    done:Boolean,
-    userId: ObjectId
+    title: String, // todo title
+    done: Boolean, // completion status
+    userId: ObjectId, // reference to the user who created the todo
+    createAt: {type: Date, default: Date.now}, // creation timestamp
+    deadline: {type: Date}, // optional deadline
 })
 
-const UserModel = mongoose.model('user', User)
-const TodoModel = mongoose.model('todos', Todo)
+// Create Mongoose models for User and Todo
+const UserModel = mongoose.model("users", User);
+const TodoModel = mongoose.model("todos", Todo);
 
+// Export the models for use in other files
 module.exports = {
-    UserModel: UserModel,
-    TodoModel:TodoModel
+    UserModel,
+    TodoModel,
 }
