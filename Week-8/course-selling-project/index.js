ce186fdb26ea5e8a86f9ade9ddde0681c54589a1
Shivang Upadhyay <shivang14071993@gmail.com>
2025-09-03 17:11:32 +0530

    Week-8 Update

--- Week-8/course-selling-project/index.js
+++ Week-8/course-selling-project/index.js
@@ -3,4 +3,11 @@ const app = express();
 app.use(express.json());
 const PORT = 3000;
 
- 
+import { userRouter } from './routes/user';
+
+app.use('/user', userRouter)
+
+
+ app.listen(PORT, ()=>{
+    console.log(`Example app listening on port${PORT}`)
+ })
---
+++ Week-8/course-selling-project/routes/user.js
@@ -0,0 +1,19 @@
+const {Router} = require('express');
+
+const userRouter  = Router();
+
+userRouter.post('/signup', (req, res)=>{
+
+})
+
+userRouter.post('/login',(req, res)=>{
+
+})
+
+userRouter.get('/purchases', (req, res)=>{
+
+})
+
+module.exports = {
+    userRouter: userRouter
+}
