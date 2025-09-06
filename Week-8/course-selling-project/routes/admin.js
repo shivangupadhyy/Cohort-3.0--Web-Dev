f0d5bb2961f6ad40d67977d9b6b2d734c7754579
Shivang Upadhyay <shivang14071993@gmail.com>
2025-09-06 18:24:19 +0530

    Week-8 Update

--- Week-8/course-selling-project/routes/admin.js
+++ Week-8/course-selling-project/routes/admin.js
@@ -1,34 +1,107 @@
-const {Router} = require('express');
+const { Router } = require("express");
 
 const adminRouter = Router();
-const {AdminModel} = require("../db")
+const { AdminModel, CourseModel } = require("../db");
 
-adminRouter.post('/signup',(req, res)=>{
-    res.json({
-        message: "SignUp successfully "
-    })
-})
+const { adminMiddleware } = require("../middleware/admin");
 
-adminRouter.post('/login',(req, res)=>{
-    res.json({
-        message: "Login successfully"
-    })
-})
+const JWT_ADMIN_PASSWORD = "ilove100xadminpassword";
 
-adminRouter.post('/',(req, res)=>{
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const zod = require("zod");
 
-})
+adminRouter.post("/signup", async (req, res) => {
+  const requireBody = zod.object({
+    email: zod.string().email().min(5), // email must be valid format and at least 5 character
+    password: zod.string().min(5), // password must be at least 5 character
+    firstName: zod.string().min(5), // first name must be at least 5 character
+    lastName: zod.string().min(3), // lastname must be at least 3 character
+  });
 
-adminRouter.put('/',(req, res)=>{
+  const parseDataWithSuccess = requireBody.safeParse(req.body);
 
-})
+  if (!parseDataWithSuccess.success) {
+    return res.json({
+      message: "Incorrect data format",
+      error: parseDataWithSuccess.error,
+    });
+  }
 
-adminRouter.get('/bulk', (req, res)=>{
+  const { email, password, firstName, lastName } = req.body;
 
-})
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    await AdminModel.create({
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: "You are already signup!",
+    });
+  }
+
+  res.json({
+    message: "You have been Signup Successfully!",
+  });
+});
+
+adminRouter.post("/signin", async (req, res) => {
+  const requireBody = zod.object({
+    email: zod.string().min(5).email(),
+    password: zod.string().min(5),
+  });
+
+  const parseDataWithSuccess = requireBody.safeParse(req.body);
+
+  if (!parseDataWithSuccess.success) {
+    return res.json({
+      message: "incorrect Data format",
+      error: parseDataWithSuccess.error,
+    });
+  }
+
+  const { email, password } = req.body;
+
+  const admin = await AdminModel.findOne({
+    email,
+  });
+  if (!admin) {
+    return res.status(403).json({
+      message: "Invalid credentails",
+    });
+  }
+
+  const passwordMatch = await bcrypt.compare(password, admin.password);
+
+  if (passwordMatch) {
+    const token = jwt.sign(
+      {
+        id: admin._id,
+      },
+      JWT_ADMIN_PASSWORD
+    );
+
+    res.status(200).json({
+      token: token,
+    });
+  } else {
+    res.status(403).json({
+      message: "Invalid credentials!",
+    });
+  }
+});
+
+adminRouter.post("/course", (req, res) => {});
+
+adminRouter.put("/course", (req, res) => {});
+
+adminRouter.get("/course/bulk", (req, res) => {});
 
 module.exports = {
-    adminRouter: adminRouter
-}
-
-
+  adminRouter: adminRouter,
+};
--- Week-8/course-selling-project/routes/user.js
+++ Week-8/course-selling-project/routes/user.js
@@ -1,20 +1,110 @@
 const {Router} = require('express');
-const {UserModel} = require("../db")
+const {UserModel,PurchaseModel, CourseModel} = require("../db");
+const {userMiddleware} = require('../middleware/user');
+const JWT_USER_PASSWORD = "ilove100xdevsPassword";
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
+const zod = require('zod');
 
 const userRouter  = Router();
 
-userRouter.post('/signup', (req, res)=>{
-
+userRouter.post('/signup',async (req, res)=>{
+const requireBody = zod.object({
+    email: zod.string().email().min(5),
+    password: zod.string().min(8),
+    firstName: zod.string().min(5),
+    lastName : zod.string().min(3),
 })
 
-userRouter.post('/login',(req, res)=>{
+const parseDataWithSuccess = requireBody.safeParse(req.body)
 
+if(!parseDataWithSuccess.success){
+    return res.json({
+        message: "Invalid Correct Format",
+        error: parseDataWithSuccess.error,
+    })
+}
+
+const {email, password, firstName, lastName} = req.body;
+
+const hashedPassword = await bcrypt.hash(password, 10);
+try{
+    await UserModel.create({
+        email,
+        password: hashedPassword,
+        firstName,
+        lastName,
+    })
+}catch(error){
+    return res.status(400).json({
+        message: "You have been already Signup",
+    })
+}
+
+res.status(201).json({
+    message: "Signup Successfully!",
+})
 })
 
-userRouter.get('/purchases', (req, res)=>{
-res.json({
-   
+userRouter.post('/signin', async(req, res)=>{
+    const requireBody = zod.object({
+        email: zod.string().email().min(4),
+        password: zod.string().min(8),
+    })
+
+    const parseDataWithSuccess = requireBody.safeParse(req.body);
+
+    if(!parseDataWithSuccess.success){
+        return res.json({
+            message: "Incorrect data format",
+            error: parseDataWithSuccess.error,
+        })
+    }
+
+    const{email, password} = req.body;
+
+    const user = await UserModel.findOne({
+       email: email,
+    })
+
+    if(!user){
+        return res.status(403).json({
+            message: "Invalid credentials!",
+        })
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if(passwordMatch){
+        const token = jwt.sign({
+            id : user._id
+        }, JWT_USER_PASSWORD)
+
+        res.status(200).json({
+            token: token,
+        })
+    }else{
+        res.status(403).json({
+            message: "invalid credentails",
+        })
+    }
 })
+
+userRouter.get('/purchases', userMiddleware,async(req, res)=>{
+    const userId = req.userId;
+    const purchases = await PurchaseModel.find({
+        userId: userId,
+    })
+
+    if(!purchases){
+        return res.status(404).json({
+            message: "No purchase found",
+        })
+    }
+
+    const purchaseCourseInfo = purchases.map((purchase)=> purchases.courseId)
+
+    
 })
 
 module.exports = {
