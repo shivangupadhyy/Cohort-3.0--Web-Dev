cd75c468c14fa9ac760eed17eeb91554daef0e09
Shivang Upadhyay <shivang14071993@gmail.com>
2025-09-10 23:28:24 +0530

    Week-8  update

--- Week-8/course-selling-project/routes/courses.js
+++ Week-8/course-selling-project/routes/courses.js
@@ -1,15 +1,32 @@
 const {Router} = require("express");
-const {CourseModel} = require("../db")
-const {PurchaseModel} = require("../db")
+const {userMiddleware} = require('../middleware/user')
+const {PurchaseModel, CourseModel} = require('../db')
 
 const courseRouter = Router();
 
-courseRouter.post('/purchase', (req, res)=>{
+courseRouter.post('/purchase',userMiddleware ,async(req, res)=>{
+    const userId = req.id;
+    const courseId = req.body.courseId;
+
+    await PurchaseModel.create({
+        userId,
+        courseId,
+    })
+
+    res.json({
+        Message: "You have successfully bought the course "
+    })
+
+
 
 })
 
-courseRouter.get('/preview', (req, res)=>{
+courseRouter.get('/preview', async(req, res)=>{
+    const courses = CourseModel.find({})
 
+    req.json({
+        courses,
+    })
 })
 
 module.exports = {
