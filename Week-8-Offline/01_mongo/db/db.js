a024995a9aa17fb898fda93eb741ae9b781b786c
Shivang Upadhyay <shivang14071993@gmail.com>
2025-09-12 20:50:49 +0530

    Week-8 Offline update

--- Week-8-Offline/01_mongo/db/db.js
+++ Week-8-Offline/01_mongo/db/db.js
@@ -1,27 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const ObjectId = mongoose.ObjectId;
 
-mongoose.connect("mongodb+srv://shivang14071993:4FfCt1jEXdf1M7OH@cluster0.rajcklb.mongodb.net/coursera-app");
+mongoose.connect("mongodb+srv://shivang14071993:4FfCt1jEXdf1M7OH@cluster0.rajcklb.mongodb.net/test-week8-app");
 
-const adminSchema = new mongoose.Schema({
+const adminSchema = new Schema({
     username : {type: String, unique: true},
     password: String,
 })
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
     username: {type: String, uniqure: true},
     password : String,
 
-    purchaseCourse : [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Course"
-        }
-    ]
+    // purchaseCourse : [
+    //     {
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: "Course"
+    //     }
+    // ]
 })
 
-const couresSchema = new mongoose.Schema({
+const couresSchema = new Schema({
     title : String,
     description : String,
     imageUrl : String,
--- Week-8-Offline/01_mongo/routes/user.js
+++ Week-8-Offline/01_mongo/routes/user.js
@@ -1,39 +1,95 @@
 const express = require('express');
 const { UserModel } = require('../db/db');
+const zod = require("zod")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
+const { model } = require('mongoose');
+const JWT_SECRET = "ilove100xdev"
 const app = express();
 app.use(express.json());
 
-const router = express.Router();
+const userRouter = express.Router();
 
 
-router.post('/signup', async(req, res)=>{
+userRouter.post('/signup', async(req, res)=>{
+
+    const requireBody = zod.object({
+        username : zod.string().min(5),
+        password : zod.string().min(5),
+    })
+
+    const paresDataWithSuccess = requireBody.safeParse(req.body);
+
+    if(!paresDataWithSuccess.success){
+        res.json({
+            message: "Invalid credentails",
+            error : paresDataWithSuccess.error,
+        })
+    }
     const username  = req.body.username;
     const password = req.body.password;
 
-   const existingUser = await UserModel.find({ username });
+    const hashedPassword = await bcrypt.hash(password, 10)
 
-   if(existingUser){
-    res.status(400).json({
-        message : "You have signup already",
+   try {
+    await UserModel.create({
+    username,
+    password : hashedPassword,
+   })
+    } catch (error) {
+    return res.status(400).json({
+        message : "You have already signup!"
     })
    }
 
-   await UserModel.create({
-    username,
-    password,
+   res.status(200).json({
+    message: "You have signedUp successfully!"
    })
-   res.json({
-    message: "You have Signup successfully!",
-   })
+   
 })
 
-router.post("/signin", (req, res)=>{
+userRouter.post("/signin", async(req, res)=>{
+
+    const requireBody = zod.object({
+        username : zod.string().min(3),
+        password : zod.string().min(5),
+    })
+
+    const paresDataWithSuccess = requireBody.safeParse(req.body);
+    if(!paresDataWithSuccess.success){
+        return res.status(400).json({
+            message : "invalid Data format",
+            error : paresDataWithSuccess.error,
+        })
+    }
     const username  = req.body.username;
     const password = req.body.password;
 
+    const user = await UserModel.findOne({username})
+    console.log(user)
+    if(!user){
+        return res.status(403).json({
+            message: "Invalid credentials!"
+        })
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if(passwordMatch){
+        const token = jwt.sign({
+            id : user.username
+        }, JWT_SECRET)
     
+
+   res.status(200).json({
+    token: token,
+    message: "You have been signin successfully!",
+   })
+   }else{
+    return res.status(403).json({
+        message: "invalid credentails",
+    })
+   }
 })
 
-
-
-app.listen(3000);
+module.exports = userRouter;
