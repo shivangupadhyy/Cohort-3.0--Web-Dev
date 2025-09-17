6c4da228d4abbe3ddb28ded0eff6d13a43c0ad2f
Shivang Upadhyay <shivang14071993@gmail.com>
2025-09-17 23:28:49 +0530

    Week-9 Update

--- Week-9/01basicsreactjs/src/App.jsx
+++ Week-9/01basicsreactjs/src/App.jsx
@@ -1,5 +1,4 @@
 function App() {
-  return <h1>Hello World</h1>;
+  return <h1>Hello World shivang</h1>;
 }
-
 export default App;
---
+++ Week-9/01basicsreactjs/src/chai.js
@@ -0,0 +1,7 @@
+function chai(){
+    return (
+        <h3>Chai aur code</h3>
+    )
+}
+
+export default chai
