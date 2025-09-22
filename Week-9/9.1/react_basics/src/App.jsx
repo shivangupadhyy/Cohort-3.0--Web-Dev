f6b8e178c0876dc45114f820bd5a5d5362d35b58
Shivang Upadhyay <shivang14071993@gmail.com>
2025-09-22 21:09:22 +0530

    Mini Project on backend for practice

--- Week-9/9.1/react_basics/src/App.jsx
+++ Week-9/9.1/react_basics/src/App.jsx
@@ -19,29 +19,39 @@ function App() {
     // let newArr = [...todos];   ← Modern & cleaner way
 
     // Creating an empty array manually
-    let newArr = [];
+    // let newArr = [];
 
     // Copying all old todos into newArr using a loop
-    for (let i = 0; i < todos.length; i++) {
-      newArr.push(todos[i]);
-    }
+    // for (let i = 0; i < todos.length; i++) {
+    //   newArr.push(todos[i]);
+    // }
 
     // Adding a new todo to newArr
-    newArr.push({
-      // Reading values directly from input boxes using document.getElementById
-      // (not recommended in React, but works for now)
-      title: document.getElementById("title").value,
-      description: document.getElementById("description").value,
-      done: true, // You’re marking new todos as done automatically
-    });
+    // newArr.push({
+    //   // Reading values directly from input boxes using document.getElementById
+    //   // (not recommended in React, but works for now)
+    //   title: document.getElementById("title").value,
+    //   description: document.getElementById("description").value,
+    //   done: true, // You’re marking new todos as done automatically
+    // });
 
     // Updating the state → tells React to re-render with new list
-    setTodos(newArr);
+    // setTodos(newArr);
+
+
+    setTodos([
+      ...todos,
+      {
+        title: document.getElementById("title").value,
+        description: document.getElementById("description").value,
+        done: false,
+      },
+    ]);
   }
 
   // JSX (UI code)
   return (
-    <>
+    <div>
       <h1>Todo App</h1>
 
       {/* Input for Todo Title */}
@@ -60,9 +70,34 @@ function App() {
       <br />
 
       {/* Displaying todos array as JSON string on screen */}
-      {JSON.stringify(todos)}
-    </>
+      {/* {JSON.stringify(todos)} */}
+
+
+      {
+        todos.map((todo, index) => {
+          return (
+            <Todo
+              key={index}
+              title={todo.title}
+              description={todo.description}
+              done={todo.done}
+            />
+          );
+        })
+      }
+    </div>
   );
+}
+
+function Todo(props) {
+
+  return(
+    <div>
+    <h3>{props.title}</h3>
+    <h3>{props.description}</h3>
+    <p>{props.done ? "Task is Done" : "Task is Not Done"}</p>
+    </div>
+  )
 }
 
 export default App;
--- Week-9/9.2/reacthooks/src/App.jsx
+++ Week-9/9.2/reacthooks/src/App.jsx
@@ -1,38 +1,59 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
+
+//explain conditional renderding
 function App() {
   return (
     <>
       <div>
-        <h1>Hello world</h1>
         <Counter></Counter>
       </div>
     </>
   )
 }
 
+//mounting , re-rendering, unmounting
+
 function Counter(){
   const [ count , setCount] = useState(0);
 
-  function increaseCount() {
-   setCount(count + 1)
-  }
+  console.log("Inside Counter Componennet")
 
-  function decreasecount(){
-    setCount(count - 1)
-  }
+  // useEffect(function(){
+  //   setInterval(function(){
+  //   setCount( function(count){
+  //     return count +1 ;
+  //   })
+  // }, 1000)
+  // console.log("mounted")
+  // }, [])
 
-  function resetCount (){
-    setCount(0)
-  }
+  useEffect(function(){
+    setInterval(function(){
+      setCount(count => count + 1 )
+    }, 1000);
+  }, []) // what is dependecy array, cleanup, fetch inside use effect with examplpe explain
+  
+
+  // function increaseCount() {
+  //  setCount(count + 1)
+  // }
+
+  // function decreasecount(){
+  //   setCount(count - 1)
+  // }
+
+  // function resetCount (){
+  //   setCount(0)
+  // }
 
 
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={increaseCount}>InCrease Count</button>
-      <button onClick={decreasecount}>Decrease Count</button>
-      <button onClick={resetCount}>Reset Count</button>
+      {/* <button onClick={increaseCount}>InCrease Count</button> */}
+      {/* <button onClick={decreasecount}>Decrease Count</button>
+      <button onClick={resetCount}>Reset Count</button> */}
     </div>
   )
   
