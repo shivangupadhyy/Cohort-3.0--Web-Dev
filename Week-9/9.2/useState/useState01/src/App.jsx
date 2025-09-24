fde8f5b5d8783ddc3a55068d6d7d92c358e9598c
Shivang Upadhyay <shivang14071993@gmail.com>
2025-09-24 21:53:41 +0530

    Week-9 Update

--- Week-9/9.2/useState/useState01/src/App.jsx
+++ Week-9/9.2/useState/useState01/src/App.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+
 function App() {
   return (
+    <div style={{ backgroundColor: "#dfe6e9", height: "100vh" }}>
+      <ToggleMessage></ToggleMessage>
+    </div>
+  )
+}
+
+  //When the value of a state variable changes,
+  //the component that uses the state variable re-render
+
+  //the component isnot re-rendering
+  //because we havent used a state variable
+
+
+
+// const ToggleMessage = () =>{
+  
+//   const[isvisible, setIsVisible]  = useState(true)
+
+//   function toggle(){
+//     setIsVisible(!isvisible); //false => true, true => false
+//   }
+
+
+const ToggleMessage = () =>{
+  
+  const[count, setCount]  = useState(0)
+
+  function increment(){
+    setCount(count + 1); //false => true, true => false
+  }
+
+  return (
     <div>
-      Hello World
+      <button onClick={increment}>
+        increase count
+      </button>
+      {/* {isvisible && <p>This message is conditional rendered!</p>} */}
+      {count}
     </div>
   )
 }
