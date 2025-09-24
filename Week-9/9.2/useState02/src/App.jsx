8525f6f355b909811b2f8063153b9c43915e9fd1
Shivang Upadhyay <shivang14071993@gmail.com>
2025-09-24 22:05:52 +0530

    Week-9 Update

--- Week-9/9.2/useState02/src/App.jsx
+++ Week-9/9.2/useState02/src/App.jsx
@@ -1,6 +1,25 @@
+import { PostComponent } from "./PostComponent"
+
 function App() {
+  
+  function addPost(){
+
+  }
   return (
-    <div>Hello world</div>
+   <div style={{background: "#dfe6e9", height: "100vh"}}>
+    <button onClick={addPost}>Add post</button>
+    <div style={{display: "flex", justifyContent: "center"}}>
+      <div>
+        <PostComponent
+        name={"Shivang"}
+        subtitle={"1100 follower"}
+        time={"2m ago"}
+        image={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"}
+        description={"what to know how to win big? check out how these folks won $6000 bounties. "}
+        ></PostComponent>
+      </div>
+    </div>
+   </div>
   )
 }
 
