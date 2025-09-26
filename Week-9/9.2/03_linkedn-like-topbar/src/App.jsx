5be576d56973248f01f34c0bca9264f62f37071e
Shivang Upadhyay <shivang14071993@gmail.com>
2025-09-26 23:46:18 +0530

    Week-9 Update

--- Week-9/9.2/03_linkedn-like-topbar/src/App.jsx
+++ Week-9/9.2/03_linkedn-like-topbar/src/App.jsx
@@ -3,14 +3,17 @@ import { useEffect, useState } from "react"
 function App() {
   const [currentTab, setCurrentTab] = useState(1); // number
   const [tabData, setTabData] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(function(){
     // send a backend request to get data for this tab
     // console.log("send request to backend to get data for tab " + currentTab);
+    setLoading(true)
     fetch('https://jsonplaceholder.typicode.com/todos/1' + currentTab)
     .then(async res => {
       const json = await res.json();
-      setCurrentTab(json);
+      setTabData(json);
+      setLoading(false)
     });
 
   }, [currentTab]);
@@ -21,8 +24,8 @@ function App() {
       <button onClick={() => setCurrentTab(2)} style={{color: currentTab === 2 ? "red" : "black"}}>Todo #2</button>
       <button onClick={() => setCurrentTab(3)} style={{color: currentTab === 3 ? "red" : "black"}}>Todo #3</button>
       <button onClick={() => setCurrentTab(4)} style={{color: currentTab === 4 ? "red" : "black"}}>Todo #4</button>
-
-      {tabData.title}
+      <br></br>
+      <div style={{backgroundColor: "black", color: "white", borderRadius: 5, paddingLeft:50}}>{ loading ? "loading..." : tabData.title}</div>
     </div>
   )
 }
