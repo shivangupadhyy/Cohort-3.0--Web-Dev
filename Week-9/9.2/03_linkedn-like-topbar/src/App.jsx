d2385cd83bb8c44cf01e6c7e4fd59c053e35ba36
Shivang Upadhyay <shivang14071993@gmail.com>
2025-09-26 23:25:49 +0530

    Week-9 Update

--- Week-9/9.2/03_linkedn-like-topbar/src/App.jsx
+++ Week-9/9.2/03_linkedn-like-topbar/src/App.jsx
@@ -1,22 +1,28 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
+  const [currentTab, setCurrentTab] = useState(1); // number
+  const [tabData, setTabData] = useState({});
 
-  const [ currentTab, setCurrentTab] = useState()
+  useEffect(function(){
+    // send a backend request to get data for this tab
+    // console.log("send request to backend to get data for tab " + currentTab);
+    fetch('https://jsonplaceholder.typicode.com/todos/1' + currentTab)
+    .then(async res => {
+      const json = await res.json();
+      setCurrentTab(json);
+    });
+
+  }, [currentTab]);
+
   return (
     <div>
-      <button onClick={function (){
-        setCurrentTab("feed")
-      }} style={{color: currentTab == "feed" ? "red" : "black"}}>Feed</button>
-      <button onClick={function(){
-        setCurrentTab("notifications")
-      }} style={{color: currentTab == "notifications" ? "red" : "black"}}>Notification</button>
-      <button onClick={function(){
-        setCurrentTab("messages")
-      }} style={{color: currentTab == "messages" ? "red" : "black"}}>Message</button>
-      <button onClick={function(){
-        setCurrentTab("jobs")
-      }} style={{color: currentTab == "jobs" ? "red" : "black"}}>Jobs</button>
+      <button onClick={() => setCurrentTab(1)} style={{color: currentTab === 1 ? "red" : "black"}}>Todo #1</button>
+      <button onClick={() => setCurrentTab(2)} style={{color: currentTab === 2 ? "red" : "black"}}>Todo #2</button>
+      <button onClick={() => setCurrentTab(3)} style={{color: currentTab === 3 ? "red" : "black"}}>Todo #3</button>
+      <button onClick={() => setCurrentTab(4)} style={{color: currentTab === 4 ? "red" : "black"}}>Todo #4</button>
+
+      {tabData.title}
     </div>
   )
 }
