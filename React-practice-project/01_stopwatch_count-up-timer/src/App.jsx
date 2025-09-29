603afd943c81f8cad9209ce596fb96c6c80eb02a
Shivang Upadhyay <shivang14071993@gmail.com>
2025-09-29 10:21:12 +0530

    Week-9 Update

--- React-practice-project/01_stopwatch_count-up-timer/src/App.jsx
+++ React-practice-project/01_stopwatch_count-up-timer/src/App.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react"
+
 function App() {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
   return (
-    <div>Hello world</div>
+    <>
+    <h1>01-StopWatch</h1>
+    <div>
+      <span>{"0" + Math.floor((time/60000) % 60)} :</span>
+      <span>{"0" + Math.floor((time/1000) % 60)} :</span>
+      <span>{"0" + Math.floor((time/10) % 100)} :</span>
+    </div>
+    <div>
+      <button onClick={() => {setRunning(true)}}>Start</button>
+      <button onClick={()=> {setRunning(false)}}>Stop</button>
+      <button onClick={()=>{setTime(0)}}>Reset</button>
+    </div>
+    </>
   )
 }
 
