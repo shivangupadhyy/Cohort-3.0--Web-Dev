71d428bf5017dc8d84e3769f5fd9374cba659d3b
Shivang Upadhyay <shivang14071993@gmail.com>
2025-09-29 20:52:27 +0530

    Code Upadate

--- React-practice-project/01_stopwatch_count-up-timer/src/App.jsx
+++ React-practice-project/01_stopwatch_count-up-timer/src/App.jsx
@@ -1,8 +1,20 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
   const [time, setTime] = useState(0);
   const [running, setRunning] = useState(false);
+
+  useEffect(()=>{
+    let interval;
+    if(running){
+      interval = setInterval(()=>{
+        setTime((prevTime) = prevTime + 10);
+      }, 10)
+    }else if(!running){
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running])
   return (
     <>
     <h1>01-StopWatch</h1>
