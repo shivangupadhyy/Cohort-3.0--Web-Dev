4633a0dc633a72898f3054d915dff90cb1fef49c
Shivang Upadhyay <shivang14071993@gmail.com>
2025-09-29 21:02:20 +0530

    Code Upadate

--- React-practice-project/01_stopwatch_count-up-timer/src/App.jsx
+++ React-practice-project/01_stopwatch_count-up-timer/src/App.jsx
@@ -1,35 +1,58 @@
-import { useEffect, useState } from "react"
+import './index.css'
+import { useEffect, useState } from "react";
 
 function App() {
   const [time, setTime] = useState(0);
   const [running, setRunning] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     let interval;
-    if(running){
-      interval = setInterval(()=>{
-        setTime((prevTime) = prevTime + 10);
-      }, 10)
-    }else if(!running){
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running) {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
-  }, [running])
+  }, [running]);
   return (
     <>
-    <h1>01-StopWatch</h1>
-    <div>
-      <span>{"0" + Math.floor((time/60000) % 60)} :</span>
-      <span>{"0" + Math.floor((time/1000) % 60)} :</span>
-      <span>{"0" + Math.floor((time/10) % 100)} :</span>
-    </div>
-    <div>
-      <button onClick={() => {setRunning(true)}}>Start</button>
-      <button onClick={()=> {setRunning(false)}}>Stop</button>
-      <button onClick={()=>{setTime(0)}}>Reset</button>
-    </div>
+      <h1 className='px-13'>01-StopWatch</h1>
+      <div>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)} : </span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)} : </span>
+        <span>{("0" + Math.floor((time / 10) % 100)).slice(-2)} </span>
+      </div>
+      <div>
+        {running ? (
+          <button
+            onClick={() => {
+              setRunning(false);
+            }}
+          >
+            Stop
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              setRunning(true);
+            }}
+          >
+            Start
+          </button>
+        )}
+
+        <button
+          onClick={() => {
+            setTime(0);
+          }}
+        >
+          Reset
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
