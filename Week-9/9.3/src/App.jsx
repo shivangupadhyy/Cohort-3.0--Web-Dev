bf78d87075194a60ecc5d948e7a6ed146b4a7907
Shivang Upadhyay <shivang14071993@gmail.com>
2025-10-02 16:40:31 +0530

    Week-9 Upadate

--- Week-9/9.3/src/App.jsx
+++ Week-9/9.3/src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+// const Card = ({ children }) => {
+//     return (
+//         <div style={{
+//             border: '1px solid #ccc',
+//             borderRadius: '5px',
+//             padding: '20px',
+//             margin: '10px',
+//             boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.1)',
+//         }}>
+//             {children}
+//         </div>
+//     );
+// };
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// const App = () => {
+//     return (
+//         <div>
+//             <Card>
+//                 <h2>Card Title</h2>
+//                 <input type={'text'}></input>
+//                 <p>This is some content inside the card.</p>
+//             </Card>
+//             <Card>
+//                 <h2>Another Card</h2>
+//                 <p>This card has different content!</p>
+//             </Card>
+//         </div>
+//     );
+// };
+
+// export default App;
+
+
+const App = () =>{
+
+  const todos = [{
+    title: "5km run", 
+    description : "Run RUn RUn",
+    done : false
+  }, {
+    title: "Complete Lecture",
+    description: "By today",
+    done: true,
+  }, {
+    title: "go to Gym",
+    description:" Eat Healthy",
+    done: false
+  }];
+
+  const todosComponent = todos.map(todo => <Todo title={todo.title} done={todo.done}></Todo>)
+return (
+  <div>
+    {todosComponent}
+  </div>
+)
 }
 
-export default App
+function Todo({title, description, done}){
+  return <div>
+    {title} - {done ? "Done" : "Not Done!"}
+  </div>
+}
+
+export default App;
