6e6a6a47ff716dec317a44b608aa7ad42400ac0b
Shivang Upadhyay <shivang14071993@gmail.com>
2025-10-03 23:36:55 +0530

    Week-10 Update

--- Week-10/10.1/src/App.jsx
+++ Week-10/10.1/src/App.jsx
@@ -1,6 +1,29 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 function App() {
   return (
-    <div>Hello world</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/neet/online-coaching-class-11" element={<ClassProgram/>}/>
+          <Route path="/" element={<Landing/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+function Landing(){
+  return(
+    <div>
+      Welcome to allen
+    </div>
+  )
+}
+
+function ClassProgram(){
+  return (
+    <div>NEET Program for class 12 th student</div>
   )
 }
 
