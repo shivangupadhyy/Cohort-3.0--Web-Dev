37b9147f2210888de58ce2e93a6973455e2c9dce
Shivang Upadhyay <shivang14071993@gmail.com>
2025-10-05 00:29:38 +0530

    Week-10 Update

--- Week-10/10.1/src/App.jsx
+++ Week-10/10.1/src/App.jsx
@@ -1,15 +1,26 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route, Link, useNavigate} from "react-router-dom"
 
 function App() {
   return (
     <div>
+      
       <BrowserRouter>
+      <Link to="/">Allen</Link>
         <Routes>
           <Route path="/neet/online-coaching-class-11" element={<ClassProgram/>}/>
           <Route path="/" element={<Landing/>}/>
+          <Route path="*" element={<ErrorPage/>}/>
         </Routes>
       </BrowserRouter>
     </div>
+  )
+}
+
+function ErrorPage(){
+  return(
+    <>
+    <div>Sorry page not found</div>
+    </>
   )
 }
 
@@ -22,8 +33,16 @@ function Landing(){
 }
 
 function ClassProgram(){
+  const navigate = useNavigate();
+
+  function redirectUser(){
+    navigate("/");
+  }
   return (
+    <>
     <div>NEET Program for class 12 th student</div>
+    <button onClick={redirectUser}>Go back to Landing page</button>
+    </>
   )
 }
 
