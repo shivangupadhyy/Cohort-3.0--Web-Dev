39367a68dd3111162803da19b2d46353a7a00037
Shivang Upadhyay <shivang14071993@gmail.com>
2025-10-06 13:49:00 +0530

    Week-10 Update'

--- Week-10/10.1/src/App.jsx
+++ Week-10/10.1/src/App.jsx
@@ -1,21 +1,53 @@
-import {BrowserRouter, Routes, Route, Link, useNavigate} from "react-router-dom"
+import {BrowserRouter, Routes, Route, Link, useNavigate, Outlet} from "react-router-dom"
 
 function App() {
   return (
     <div>
       
       <BrowserRouter>
-      <Link to="/">Allen</Link>
+      
         <Routes>
+         <Route path="/" element={<Layout/>}>
           <Route path="/neet/online-coaching-class-11" element={<ClassProgram/>}/>
           <Route path="/" element={<Landing/>}/>
           <Route path="*" element={<ErrorPage/>}/>
+          </Route>
         </Routes>
+        Footer | Contact Us
       </BrowserRouter>
     </div>
   )
 }
 
+
+function Layout(){
+
+  
+    const navigate = useNavigate();
+
+
+    function redirectUser(){
+      navigate('/neet/online-coaching-class-11')
+    }
+  return (
+    <div style={{height: "100vh"}}>
+      <Header/>
+    <div style={{height: "90vh", backgroundColor: "red"}}>
+      hi there
+      <button onClick={redirectUser}> Go TO Class 11 Allen Page</button>
+      <Outlet/>
+    </div>
+    </div>
+  )
+}
+
+function Header(){
+  return <div style={{backgroundColor: "black", marginBottom:15}}>
+    <Link to="/">Allen</Link>
+    <Link to="/neet/online-coaching-class-11"> Class 11</Link>
+    <Link to="/neet/online-coaching-class-12"> Class 12</Link>
+  </div>
+}
 function ErrorPage(){
   return(
     <>
