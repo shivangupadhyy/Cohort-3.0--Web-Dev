457d39f3c16b6b2c1e98da7c425f0a12ef55ff5b
Shivang Upadhyay <shivang14071993@gmail.com>
2025-10-08 13:46:35 +0530

    Week-10 Update'

--- Week-10/10.2/src/App.jsx
+++ Week-10/10.2/src/App.jsx
@@ -1,9 +1,38 @@
+import { useState } from "react"
+
 function App() {
 return (
-  <>
-  Hello world
-  </>
+  <div>
+    <LightBulb/>
+  </div>
 )
+}
+
+function LightBulb(){
+ const [bulbOn, setBulbOn] = useState(true); 
+  return <div>
+    <BulbState bulbOn ={bulbOn}/>
+    <ToggleBulbState bulbOn ={bulbOn} setBulbOn={setBulbOn}/>
+  </div>
+}
+
+function BulbState({bulbOn}){
+  
+  return <div>
+    {bulbOn ? "Bulb on" : "Bulb off"}
+  </div>
+}
+
+function ToggleBulbState({bulbOn, setBulbOn}){
+
+  function toggle(){
+    // setBulbOn(currentState => ! currentState)
+    setBulbOn(!bulbOn)
+  }
+  
+  return <div>
+    <button  onClick={toggle}>Toggle the Bulb</button>
+  </div>
 }
 
 export default App
