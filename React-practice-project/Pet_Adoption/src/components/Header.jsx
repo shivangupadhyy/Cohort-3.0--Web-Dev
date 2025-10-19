0f5d64021f544f3e35085ce95859d6957ac8ddcd
Shivang Upadhyay <shivang14071993@gmail.com>
2025-10-19 17:22:06 +0530

    Assignment Update

---
+++ React-practice-project/Pet_Adoption/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = ({message}) => {
+    return (
+    <nav style={{backgroundColor: "#c59771bd", padding:"16px 32px"}}>
+        <h1>{message}</h1>
+    </nav>
+    )
+}
+
+export default Header;
