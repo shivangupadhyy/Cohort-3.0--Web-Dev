c8681e0c83ae2eb5d5a2ef71a7573128fed1251b
Shivang Upadhyay <shivang14071993@gmail.com>
2026-01-10 00:38:46 +0530

    Week-12 started with tailwind claases

--- Week-12/tailwind-classes/src/App.jsx
+++ Week-12/tailwind-classes/src/App.jsx
@@ -1,7 +1,17 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <div className="grid grid-cols-12">
+      <div className="bg-blue-300 col-span-4">
+        child1
+      </div>
+      <div className="bg-red-300 col-span-6">
+        child2
+      </div>
+      <div className=" bg-green-300 col-span-2">
+        child3
+      </div>
+    </div>
+    </>
   )
 }
