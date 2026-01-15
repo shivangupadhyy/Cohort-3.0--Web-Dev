01f577287be053ba58754fd8a56a2d3094e30f2e
Shivang Upadhyay <shivang14071993@gmail.com>
2026-01-15 20:11:03 +0530

    Week-12 started with tailwind claases

--- Week-12/tailwind-classes/src/App.jsx
+++ Week-12/tailwind-classes/src/App.jsx
@@ -3,18 +3,10 @@ import Button from "./components/Button";
 export default function App() {
   return (
     <>
-    <div className="grid grid-cols-12">
-      <div className="col-span-12 sm:col-span-5 bg-green-300 rounded-full text-center">
-        hi
-      </div>
-      <div className="col-span-12 sm:col-span-5 bg-red-300">
-        hello
-      </div>
-      <div className="col-span-12 sm:col-span-2 bg-blue-300">
-        world
-      </div>
-      <Button>Sign Up</Button>
+    <div>
+      
     </div>
+      <Button disabled={true}>Sign Up</Button>
     </>
   )
 }
--- Week-12/tailwind-classes/src/components/Button.jsx
+++ Week-12/tailwind-classes/src/components/Button.jsx
@@ -3,8 +3,8 @@ const Button = ({
     children,
     onClick
 }) => {
-  return <div onClick={onClick}  className={`px-32 py-8text-white pointer ${disabled ? "bg-blue-200" : "bg-green-400"} `}>
-
+  return <div onClick={onClick}  className={`px-32 py-8 text-white cursor-pointer rounded-2xl text-4xl text-center my-5 mx-5 ${disabled ? "bg-blue-200" : "bg-green-400"} `}>
+{children}
   </div>
 }
 
