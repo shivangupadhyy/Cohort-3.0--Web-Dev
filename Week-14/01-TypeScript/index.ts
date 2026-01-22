0a1b8cea062682a5403d9ed12399a0fc9bb42280
Shivang Upadhyay <shivang14071993@gmail.com>
2026-01-23 00:10:35 +0530

    some changes

--- Week-14/01-TypeScript/index.ts
+++ Week-14/01-TypeScript/index.ts
@@ -5,15 +5,32 @@
 // console.log(greet("TypeScript"));
 
 
-function greet(firstName : string){// no type annotation
-    console.log("hello" + firstName);
-}
-greet("TypeScript");//valid
-greet(1); //error
+// function greet(firstName : string){// no type annotation
+//     console.log("hello" + firstName);
+// }
+// greet("TypeScript");//valid
+// greet(1); //error
 
 //number , string, any , boolean , void , null , undefined , never , object , array
-let x : any = 1; //type annotation
-x = "TypeScript";   //type inference
-x = true;// valid
-x = null;
-x = undefined;
+// let x : any = 1; //type annotation
+// x = "TypeScript";   //type inference
+// x = true;// valid
+// x = null;
+// x = undefined;
+
+
+function sum(a:number, b:number){
+    return a+b;
+}
+
+console.log(sum(3,8)); //11
+// console.log(sum(3,"8")); //error
+// console.log(sum("3","8")); //error
+
+function delayedCall(fn: () => void){
+    setTimeout(fn, 1000);
+}
+
+delayedCall(function(){
+    console.log("hello");
+})
