ac995738fcd85c09c5faf29cf7bf3fb9241541ff
Shivang Upadhyay <shivang14071993@gmail.com>
2026-01-23 12:06:45 +0530

    Week-14 part 1 done

--- Week-14/01-TypeScript/index.ts
+++ Week-14/01-TypeScript/index.ts
@@ -19,18 +19,104 @@
 // x = undefined;
 
 
-function sum(a:number, b:number){
-    return a+b;
+// function sum(a:number, b:number){
+//     return a+b;
+// }
+
+// console.log(sum(3,8)); //11
+// // console.log(sum(3,"8")); //error
+// // console.log(sum("3","8")); //error
+
+// function delayedCall(fn: () => void){
+//     setTimeout(fn, 1000);
+// }
+
+// delayedCall(function(){
+//     console.log("hello");
+// })
+
+// function sum(a:number, b:number):number{
+//     return a+b;
+// }
+
+// let ans = sum(3,8);
+
+// console.log(ans);
+
+// function delayedCall(fn: ()=> void){
+//     setTimeout(fn, 1000);
+// }
+
+// function log(){
+//     console.log("hello world");
+// }
+
+// delayedCall(log);
+
+// function greet(user: {
+//     name: string,
+//     age: number,
+//     address : {
+//         streetName: string,
+//         country : string,
+//     }
+// }) {
+//     console.log("hello" + user.name)
+// }
+
+// greet({
+//     name: "harkirat",
+//     age: 21
+// })
+
+// let user = {
+//     name: "harkirat",
+//     age: 21
+// }
+
+// greet(user)
+
+//  interface UserType{
+//     firstName: string,
+//     lastName : string,
+//     age: number
+//  }
+
+//  function greet(user: UserType){
+
+//  }
+//todo.jsx
+// interface TodoType{
+//     title : string;
+//     description: string;
+//     done : boolean;
+// }
+
+// interface TodoInput{
+//     todo: TodoType;
+// }
+
+// function Todo(props : TodoInput){
+
+// }
+
+// <Todo Todo={{
+//     title : "go to Gym",
+//     description : "Go to gym from 8-10",
+//     done : false
+// }}
+
+// />
+
+interface Manager{
+    name: string,
+    age: number
 }
 
-console.log(sum(3,8)); //11
-// console.log(sum(3,"8")); //error
-// console.log(sum("3","8")); //error
-
-function delayedCall(fn: () => void){
-    setTimeout(fn, 1000);
+interface Employee{
+    name: string,
+    department: string
 }
 
-delayedCall(function(){
-    console.log("hello");
-})
+type TeamLead = Manager & Employee;
+
