e615123265fde96fe0b13c1cf820ba8bb74e2fa0
Shivang Upadhyay <shivang14071993@gmail.com>
2026-01-28 15:16:30 +0530

    Week-14 part 3 done

--- Week-14/02-TypeScript/src/index.ts
+++ Week-14/02-TypeScript/src/index.ts
@@ -10,37 +10,116 @@
 // const age = sumOfAge({name : "Shivang", age : 20} , {name : "sachin", age : 20});
 // console.log(age)
 
-interface User{
-    id: string;
-    name : string;
-    age : number;
-    email : string;
-    password : string;
-};
+// interface User{
+//     id: string;
+//     name : string;
+//     age : number;
+//     email : string;
+//     password : string;
+// };
 
-type UpdateProps = Pick<User, 'name' | 'age' | 'email' >
+// type UpdateProps = Pick<User, 'name' | 'age' | 'email' >
 
-type UpdatePropsOptional = Partial<UpdateProps>
+// type UpdatePropsOptional = Partial<UpdateProps>
 
-function updateUser(updatedProps : UpdateProps){
+// function updateUser(updatedProps : UpdateProps){
 
-}
+// }
 
 // function updateUser(name : string, age : number, password : string){
 //     //hit the database tp update the user
 // }
 
 
-updateUser({
-    name : "asd",
-    age : 23,
-    email : "Shivang"
-})
+// updateUser({
+//     name : "asd",
+//     age : 23,
+//     email : "Shivang"
+// })
 
 
-const lastName = "Shivang";
-lastName = "upadhyay";
+// const lastName = "Shivang";
+// lastName = "upadhyay";
 
-const a = [1,2,3];
-a[0] = 4; // wecan change the internal value of the array but can not the array 
+// const a = [1,2,3];
+// a[0] = 4; // wecan change the internal value of the array but can not the array 
 // a = [1,3,4];
+
+// const obj = {
+//     name : "Shivanh",
+//     age : 25,
+//     country : "india",
+// }
+
+// obj = { // will throw and error
+
+// }
+
+// obj.name = "Shivang";
+
+// type User = {   
+//     name : string;
+//     age : number;
+// }
+
+// const user : Readonly<User> = {
+//     name : 'Shivang',
+//     age : 21,
+// }
+
+// user.age = 12;
+
+// type User = {
+//     id : string;
+//     username : string;
+// }
+
+// type Users = {
+//     [key: string] : User;
+// }
+
+
+// const users : Users = {
+//     "Shivang" : {
+//         id : 'ras@qd1',
+//         username : 'harkirar'
+//     }, 
+//     "ayuu" : {
+//         id: "ras1dre@",
+//         username : 'ayyyuuu'
+//     }
+// }
+
+// interface User{
+//     id : number;
+//     name : string;
+//     email : string;
+//     createdAt : Date;
+// }
+
+// //for a profile display , only `name` and `email`;
+
+// type UserProfile = Pick<User, 'name' | 'email'>;
+
+// const displayUserProfile = (user: UserProfile)=>{
+//     console.log(`Name : ${user.name}, Email : ${user.email}`);
+// }
+
+
+interface User{
+    id:String;
+    name : string;
+    age : string;
+    email : string;
+    password:string;
+};
+
+type UpdateProps = Pick<User, 'name' | 'age' | 'email'>
+
+type UpdatePropsOptional = Partial<UpdateProps>
+
+function updateUser(updatedProps : UpdatePropsOptional){
+    //
+}
+
+updateUser({});
