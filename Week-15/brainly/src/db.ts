68a7586ebd2864c76177aa74d79b2caf04bf1a45
Shivang Upadhyay <shivang14071993@gmail.com>
2026-01-29 19:34:26 +0530

    Week-15 part 3 done

--- Week-15/brainly/src/db.ts
+++ Week-15/brainly/src/db.ts
@@ -1,1 +1,27 @@
 //create user model and schema
+import mongoose, {model, Schema} from "mongoose";
+
+mongoose.connect("mongooseString");
+
+const UserSchema = new Schema({
+    username : {type: String, unique: true},
+    password : String
+})
+
+export const UserModel = model("User", UserSchema);
+
+const ContentSchema = new Schema({
+    title : String,
+    link : String,
+    tags: [{type: mongoose.Types.ObjectId, ref: 'Tag'}],
+    type: String,
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', require: true},
+})
+
+const LinkSchema = new Schema({
+    hash: String,
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', require: true, unique: true},
+})
+
+export const LinkModel = model("Link", LinkSchema);
+export const contentModel = model("Content", ContentSchema);
