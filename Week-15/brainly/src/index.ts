679195b295dcec2d1cee63e87f78cae68076b8f8
Shivang Upadhyay <shivang14071993@gmail.com>
2026-01-29 11:50:18 +0530

    Week-14 part 3 done

--- Week-15/brainly/src/index.ts
+++ Week-15/brainly/src/index.ts
@@ -3,3 +3,19 @@ import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 
 const app = express();
+
+app.post("/api/v1/signup", (req, res) => {
+
+})
+
+app.post("/api/v1/signin", (req, res)=>{
+
+})
+
+app.post("/appi/v1/content", (req, res)=>{
+
+})
+
+app.get("/api/v1/content", (req, res)=>{
+    
+})
