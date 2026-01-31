130e2d4594e130bbdd48f4ada34cdf0a31415d31
Shivang Upadhyay <shivang14071993@gmail.com>
2026-01-31 10:55:34 +0530

    Week-15 part 3 done

--- Week-15/brainly/src/index.ts
+++ Week-15/brainly/src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
-import { ContentModel, UserModel } from "./db.js";
+import { random } from "./utils.js";
+import { ContentModel, LinkModel, UserModel } from "./db.js";
 import { JWT_PASSWORD } from "./config.js";
 import { useMiddleware } from "./middleware.js";
+import cors from 'cors';
 const app = express();
 app.use(express.json());
 
+
 app.post("/api/v1/signup", async(req, res) => {
-    //zod validation here
+    //zod validation here and , hash the password
+
     const username =  req.body.username;
     const password = req.body.password;
 
@@ -80,7 +84,7 @@ res.json({
 app.delete("/api/v1/content",useMiddleware, async(req, res)=>{
     const contentId = req.body.contentId;
 
-    await ContentModel.deleteOne({
+    await ContentModel.deleteMany({
         _id: contentId,
         userId: new mongoose.Types.ObjectId(req.userId)
     })
@@ -90,12 +94,79 @@ app.delete("/api/v1/content",useMiddleware, async(req, res)=>{
     })
 })
 
-app.post("/api/v1/brain/share",(req, res)=>{
+app.post("/api/v1/brain/share",useMiddleware, async(req, res)=>{
+    const share  = req.body.share;
+    if(share){
+        const existingLink = await LinkModel.findOne({
+            userId: new mongoose.Types.ObjectId(req.userId)
+        });
+
+        if(existingLink){
+            res.json({
+                hash: existingLink.hash
+            })
+            return;
+        }
+
+        const hash = random(10);
+        await LinkModel.create({
+            userId : new mongoose.Types.ObjectId(req.userId),
+            hash : hash
+        })
+
+        res.json({
+            hash
+        })
+
+    }else{
+        await LinkModel.deleteOne({
+            userId : new mongoose.Types.ObjectId(req.userId),
+        })
+        res.json({
+            message : "Removed link"
+        })
+    }
 
 })
 
-app.get("/api/v1/brain/:shareLink", (req, res)=>{
+app.get("/api/v1/brain/:shareLink", useMiddleware,async(req, res)=>{
+    const hash = req.params.shareLink;
 
+    const link = await LinkModel.findOne({
+        hash
+    });
+
+    if(!link){
+        res.status(411).json({
+            message: "sorry incorrect input"
+        })
+        return;
+    }
+
+    const content = await ContentModel.find({
+        userId : link.userId
+    })
+
+
+    console.log(link);
+    const user = await UserModel.findOne({
+        _id : link.userId
+    })
+
+    if(!user){
+        res.status(411).json({
+            message: "user not found , error should ideally not happen"
+        })
+        return;
+    }
+
+
+    res.json({
+        username : user.username,
+        content: content
+    })
+
+    
 })
 
 app.listen(3000, () => console.log('Server running at http://localhost:3000'));
