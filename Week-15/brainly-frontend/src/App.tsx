bd36350d0a81c4d99a9e140039924d2ce1433942
Shivang Upadhyay <shivang14071993@gmail.com>
2026-02-03 13:20:47 +0530

    Week-15 button added

--- Week-15/brainly-frontend/src/App.tsx
+++ Week-15/brainly-frontend/src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
     return (
         <div className="p-8">
             {/* <SocialFlipButton /> */}
-            <Button startIcon={<PlusIcon/>} variant="primary" size="sm" text="Share" onClick={() => alert('Clicked!')} />
+            <Button startIcon={<PlusIcon size="lg"/>} variant="primary" size="sm" text="Share" onClick={() => alert('Clicked!')} />
             <Button variant="secondary" size="md" text="Add Content" onClick={() => alert('Cancelled!')} />
             <Button variant="secondary" size="lg" text="Add Content" onClick={() => alert('Cancelled!')} />
         </div>
