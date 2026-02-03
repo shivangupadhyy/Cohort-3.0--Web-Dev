bfcb642e7f0c676433a45a9c7bd13b2e44edddd9
Shivang Upadhyay <shivang14071993@gmail.com>
2026-02-03 11:22:51 +0530

    Week-15 part 3 done

--- Week-15/brainly-frontend/src/components/ui/Button.tsx
+++ Week-15/brainly-frontend/src/components/ui/Button.tsx
@@ -1,6 +1,21 @@
 export interface ButtonProps {
-    variant : "primary | secondary";
-    size: "sm" | "md" | "lg";
-    text : string;
-    startIncon
+    variant: 'primary' | 'secondary' | 'tertiary';
+    size: 'sm' | 'md' | 'lg';
+    onClick: () => void;
+    text: string;
+    startIcon?: any;
+    endIcon?: any;
 }
+
+export const Button = (props : ButtonProps) =>{
+    // props.variant
+    // props.size
+    // props.onClick
+    // props.text
+    // props.startIcon
+    // props.endIcon
+    return <button>Click Me</button>
+}
+
+<Button variant="primary" size="md" onClick={() => alert('Clicked!')} text="Submit"/>
+
