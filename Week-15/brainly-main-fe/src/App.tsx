1f222a1c9eb4938bddb27bcfd20c369b0a66d250
Shivang Upadhyay <shivang14071993@gmail.com>
2026-02-05 10:42:30 +0530

    Some major changes made

--- Week-15/brainly-main-fe/src/App.tsx
+++ Week-15/brainly-main-fe/src/App.tsx
@@ -1,12 +1,14 @@
 
 import { Button } from "./components/Button"
+import { PlusIcon } from "./icons/PlusIcon"
+import { ShareIcon } from "./icons/ShareIcon"
 
 function App() {
   return (
  <>
-  <div className="bg-red-300">
-    <Button variant="primary" text="Add Content"></Button>
-    <Button variant="secondary" text="Share"></Button>
+  <div>
+    <Button variant="primary" text="Add Content" startIcon={PlusIcon}></Button>
+    <Button variant="secondary" text="Share" startIcon={ShareIcon}></Button>
   </div>
  </>
   
--- Week-15/brainly-main-fe/src/components/Button.tsx
+++ Week-15/brainly-main-fe/src/components/Button.tsx
@@ -7,13 +7,15 @@ interface ButtonProps{
 }
 
 const variantClasses = {
-    primary : "bg-purple-600 text-white",
-    secondary : "bg-purple-200 text-purple-400"
+    primary : "bg-purple-600 text-white ",
+    secondary : "bg-purple-200 text-purple-600"
 }
+const defaultStyles = "px-4 py-2 rounded-md font-light";
 
 export function Button({variant , text, startIcon}: ButtonProps){
 
-    return <button className={variantClasses[variant]}>
+    return <button className={variantClasses[variant] + " " + defaultStyles} >
+        {startIcon}
         {text}
     </button>
 }
