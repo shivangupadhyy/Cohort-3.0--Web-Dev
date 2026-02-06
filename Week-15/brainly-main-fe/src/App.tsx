f923c2db1c1725ca8f4fd9467b1e94645613f6de
Shivang Upadhyay <shivang14071993@gmail.com>
2026-02-06 10:36:35 +0530

    Some major changes made

--- Week-15/brainly-main-fe/src/App.tsx
+++ Week-15/brainly-main-fe/src/App.tsx
@@ -6,14 +6,19 @@ import { ShareIcon } from "./icons/ShareIcon"
 
 function App() {
   return (
- <>
-  <div>
-    <Button variant="primary" text="Add Content" startIcon={<PlusIcon/>}></Button>
+ 
+  <div className="p-4">
+    <div className="flex justify-end gap-4">
+      <Button variant="primary" text="Add Content" startIcon={<PlusIcon/>}></Button>
     <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon/>}></Button>
+    </div>
+  
+  <div className="flex gap-4">
+    <Card type="twitter" link="https://x.com/manojdotdev/status/2019379154341511615?s=20" title="First Twitte"/>
+  <Card type="youtube" link="https://www.youtube.com/watch?v=6g6LErEaF-s" title="First Youtube"/>
   </div>
-  <Card type="twitter" link="https://x.com/manojdotdev/status/2019379154341511615?s=20" title="First Twitte"/>
-  <Card type="youtube" link="https://www.youtube.com/watch?v=ssYt09bCgUY" title="First Youtube"/>
- </>
+  </div>
+ 
   
   )
 }
