480e325638b917cad94c8554fdfbb72caf97afd3
Shivang Upadhyay <shivang14071993@gmail.com>
2026-02-06 10:18:08 +0530

    Some major changes made

--- Week-15/brainly-main-fe/src/App.tsx
+++ Week-15/brainly-main-fe/src/App.tsx
@@ -1,5 +1,6 @@
 
 import { Button } from "./components/Button"
+import { Card } from "./components/Card"
 import { PlusIcon } from "./icons/PlusIcon"
 import { ShareIcon } from "./icons/ShareIcon"
 
@@ -10,6 +11,8 @@ function App() {
     <Button variant="primary" text="Add Content" startIcon={<PlusIcon/>}></Button>
     <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon/>}></Button>
   </div>
+  <Card type="twitter" link="https://x.com/manojdotdev/status/2019379154341511615?s=20" title="First Twitte"/>
+  <Card type="youtube" link="https://www.youtube.com/watch?v=ssYt09bCgUY" title="First Youtube"/>
  </>
   
   )
