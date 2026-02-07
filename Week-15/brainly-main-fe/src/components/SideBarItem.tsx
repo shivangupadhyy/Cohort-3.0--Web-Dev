c88f78b0d0b8779d4eb7fc9fac21c36e04ea71ff
Shivang Upadhyay <shivang14071993@gmail.com>
2026-02-07 11:21:38 +0530

    Some major changes made

--- Week-15/brainly-main-fe/src/components/SideBarItem.tsx
+++ Week-15/brainly-main-fe/src/components/SideBarItem.tsx
@@ -1,6 +1,12 @@
-    
-    
-    
-    
-    
-    a
+import type { ReactElement } from "react";
+
+export function SideBarItem({text, icon} :{
+    text : string;
+    icon : ReactElement
+}) {
+    return (
+        <div className="flex">
+            {icon} {text}
+        </div>
+    )
+}
--- Week-15/brainly-main-fe/src/components/Sidebar.tsx
+++ Week-15/brainly-main-fe/src/components/Sidebar.tsx
@@ -1,7 +1,14 @@
+import { Twitter } from "../icons/TwitterIcon";
+import { YoutubeIcon } from "../icons/YoutubeIcon";
+import { SideBarItem } from "./SideBarItem";
+
 export function Sidebar(){
     return (
         <div className="h-screen bg-white border-r w-72 fixed left-0 top-0 ">
-            hi there
+           <div>
+            <SideBarItem text="Twitter" icon={<Twitter/>}/>
+            <SideBarItem text="Youtube" icon={<YoutubeIcon/>}/>
+           </div>
         </div>
     )
 }
