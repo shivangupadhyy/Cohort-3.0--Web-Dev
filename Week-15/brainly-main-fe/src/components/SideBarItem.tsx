5b3349cb15392f46189c3512aa92d949b5f26bd5
Shivang Upadhyay <shivang14071993@gmail.com>
2026-02-07 19:32:47 +0530

    Some major changes made

--- Week-15/brainly-main-fe/src/components/SideBarItem.tsx
+++ Week-15/brainly-main-fe/src/components/SideBarItem.tsx
@@ -5,8 +5,13 @@ export function SideBarItem({text, icon} :{
     icon : ReactElement
 }) {
     return (
-        <div className="flex">
-            {icon} {text}
+        <div className="flex text-gray-700 py-2 cursor-pointer hover:bg-gray-100 rounded-lg items-center transition-colors duration-200 max-w-48 pl-4">
+           <div className="p-2 pt-3.5">
+            {icon}
+           </div>
+           <div className="p-2">
+            {text}
+           </div>
         </div>
     )
 }
--- Week-15/brainly-main-fe/src/components/Sidebar.tsx
+++ Week-15/brainly-main-fe/src/components/Sidebar.tsx
@@ -1,11 +1,18 @@
+import { Logo } from "../icons/Logo";
 import { Twitter } from "../icons/TwitterIcon";
 import { YoutubeIcon } from "../icons/YoutubeIcon";
 import { SideBarItem } from "./SideBarItem";
 
 export function Sidebar(){
     return (
-        <div className="h-screen bg-white border-r w-72 fixed left-0 top-0 ">
-           <div>
+        <div className="h-screen bg-white border-r w-72 fixed left-0 top-0  pl-6">
+            <h1 className="flex text-2xl pt-4 font-bold underline text-gray-700 items-center">
+                <div className="pr-4 pl-1.5 text-purple-600">
+                    <Logo/>
+                </div>
+                Brainly
+            </h1>
+           <div className="pt-4 pl-4">
             <SideBarItem text="Twitter" icon={<Twitter/>}/>
             <SideBarItem text="Youtube" icon={<YoutubeIcon/>}/>
            </div>
