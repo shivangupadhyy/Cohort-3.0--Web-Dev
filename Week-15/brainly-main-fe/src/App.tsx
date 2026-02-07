dafe41e40d064e999db947f1163f86ef267b09e1
Shivang Upadhyay <shivang14071993@gmail.com>
2026-02-07 11:12:12 +0530

    Some major changes made

--- Week-15/brainly-main-fe/src/App.tsx
+++ Week-15/brainly-main-fe/src/App.tsx
@@ -5,13 +5,16 @@ import { Card } from "./components/Card"
 import { CreateContentModal } from "./components/CreateContentModal"
 import { PlusIcon } from "./icons/PlusIcon"
 import { ShareIcon } from "./icons/ShareIcon"
+import { Sidebar } from "./components/Sidebar"
 
 function App() {
 
   const [modalOpen, setModalOpen] =  useState(false);
   return (
  
-  <div className="p-4">
+  <div>
+    <Sidebar/>
+    <div className="p-4 ml-72 min-h-screen bg-gray-100 border-2">
     <CreateContentModal open={modalOpen} onClose={() => {
       setModalOpen(false);
     }}/>
@@ -25,6 +28,7 @@ function App() {
   <div className="flex gap-4">
     <Card type="twitter" link="https://x.com/manojdotdev/status/2019379154341511615?s=20" title="First Twitte"/>
   <Card type="youtube" link="https://www.youtube.com/watch?v=6g6LErEaF-s" title="First Youtube"/>
+  </div>
   </div>
   </div>
  
