48235771232e1fe7a70ff0e8911a81121e290020
Shivang Upadhyay <shivang14071993@gmail.com>
2026-02-12 10:25:11 +0530

    Week-16 Websocket

--- Week-16/Websockets/dist/index.js
+++ Week-16/Websockets/dist/index.js
@@ -4,11 +4,18 @@ const wss = new WebSocketServer({ port: 8080 });
 wss.on("connection", function (socket) {
     console.log("user Connected");
     // socket.send("hello");
-    setInterval(() => {
-        socket.send("Current price of Solana is " + Math.random());
-    }, 500);
+    // setInterval(()=>{
+    //    socket.send("Current price of Solana is "  +  Math.random());
+    // },500)
+    // socket.on("message", (e)=>{
+    //     console.log(e.toString());
+    // })
     socket.on("message", (e) => {
         console.log(e.toString());
+        console.log(e.toString() === 'ping');
+        if (e.toString() === "ping") {
+            socket.send("pong");
+        }
     });
 });
 //# sourceMappingURL=index.js.map
