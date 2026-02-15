7c840c12f20512b210e6e657273d9103f5489996
Shivang Upadhyay <shivang14071993@gmail.com>
2026-02-15 12:45:03 +0530

    Week-16 Websocket Chat-App final code

--- Week-16/chatapp/chat-main-fe/src/App.tsx
+++ Week-16/chatapp/chat-main-fe/src/App.tsx
@@ -1,24 +1,57 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 function App() {
 
-  const [messages, setMessage] = useState("hi there");
-  useEffect(()=>{
-    const ws = new WebSocket("http://localhost:3000");
-    ws.onmessage = (event) => {
-      setMessage(m => [...m, event.data]);
-    }
-    },[])
+  const [messages, setMessage] = useState<string[]>(["hi there welcome to the chat app", "this is a message from the server"]);
+
+  const wsRef = useRef<WebSocket | null>(null);
+  useEffect(() => {
+  const socket = new WebSocket("ws://localhost:8080");
+
+  socket.onopen = () => {
+    socket.send(JSON.stringify({
+      type: "join",
+      payload: {
+        roomId: "room1"
+      }
+    }));
+
+  };
+
+  socket.onmessage = (event) => {
+    setMessage(m => [...m, event.data]);
+  };
+
+  wsRef.current = socket;
+
+  return () => {
+    socket.close();
+  };
+
+}, []);
+
 
     return (  
     <>
       <div className="h-screen bg-black">
-        <div className="h-[90vh]">
-          {messages.map(message => <div className="bg-white text-black rounded p-4 m-8">{message}</div>)}
+        <br/> <br/> <br/>
+        <div className="h-[85vh]">
+          {messages.map(message =><div className="m-8 p-1"> 
+            <span className="bg-white text-black rounded  p-4 ">{message}
+            </span>
+            </div>)}
         </div>
         <div className='w-full bg-white flex'>
-          <input className="flex-1 p-4" type="text" placeholder="Type something"></input>
-          <button className="bg-purple-600 text-white p-4">Send message</button>
+          <input id="message" className="flex-1 p-4" type="text" placeholder="Type something"></input>
+          <button onClick={()=>{
+            const message = document.getElementById("message")?.value;
+            wsRef.current.send(JSON.stringify({
+              type: "chat",
+              payload: {
+                message: message
+              }
+            }))
+          }} className="bg-purple-600 text-white p-4">Send message</button>
         </div>
       </div>
       
