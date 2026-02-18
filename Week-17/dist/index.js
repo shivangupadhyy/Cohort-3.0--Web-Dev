cf11dde344e87ba31116b4a297a8580122c94d56
Shivang Upadhyay <shivang14071993@gmail.com>
2026-02-18 13:15:44 +0530

    Week-16 Websocket Chat-App final code

--- Week-17/dist/index.js
+++ Week-17/dist/index.js
@@ -1,9 +1,22 @@
+import express from 'express';
 import { Client } from 'pg';
+const app = express();
+app.use(express.json());
 const pgClient = new Client("postgresql://neondb_owner:npg_YjZwf5Ico8SB@ep-royal-truth-a1eqgfh9-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=verify-full&channel_binding=require");
-async function main() {
-    await pgClient.connect();
-    const response = await pgClient.query("SELECT * FROM users;");
-    console.log(response);
-}
-main();
+pgClient.connect();
+app.post("/signup", async (req, res) => {
+    const { username, password, email } = req.body;
+    try {
+        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3)`;
+        await pgClient.query(insertQuery, [username, email, password]);
+        res.json({
+            message: "You have been signed up"
+        });
+    }
+    catch (err) {
+        console.error("Error inserting user:", err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+app.listen(3000);
 //# sourceMappingURL=index.js.map
--- Week-17/src/index.ts
+++ Week-17/src/index.ts
@@ -1,12 +1,29 @@
+import express from 'express'
 import {Client} from 'pg'
 
+const app = express();
+
+app.use(express.json());
 const pgClient = new Client("postgresql://neondb_owner:npg_YjZwf5Ico8SB@ep-royal-truth-a1eqgfh9-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=verify-full&channel_binding=require");
+pgClient.connect();
 
-async function main(){
-    await pgClient.connect();
-    const response = await pgClient.query("SELECT * FROM users;")
-    console.log(response);
-}
+app.post("/signup", async (req, res) => {
+    const { username, password, email } = req.body;
 
-main();
+    try {
+        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3)`;
+        await pgClient.query(insertQuery, [username, email, password]);
+
+        res.json({
+            message: "You have been signed up"
+        });
+    } catch (err) {
+        console.error("Error inserting user:", err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+})
+
+app.listen(3000);
+
+
 
