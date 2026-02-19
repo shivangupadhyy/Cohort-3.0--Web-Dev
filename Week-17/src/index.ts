cf51fb5875fd07746d721df5b8bd3765f2ec6530
Shivang Upadhyay <shivang14071993@gmail.com>
2026-02-19 13:11:35 +0530

    Week-17 Postgress

--- Week-17/src/index.ts
+++ Week-17/src/index.ts
@@ -8,11 +8,17 @@ const pgClient = new Client("postgresql://neondb_owner:npg_YjZwf5Ico8SB@ep-royal
 pgClient.connect();
 
 app.post("/signup", async (req, res) => {
-    const { username, password, email } = req.body;
+    const { username, password, email , city, country , street, pincode } = req.body;
 
     try {
-        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3)`;
-        await pgClient.query(insertQuery, [username, email, password]);
+        // insert user and return its id
+        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id`;
+        const userResult = await pgClient.query(insertQuery, [username, email, password]);
+        const userId = userResult.rows[0].id;
+
+        // insert address using returned user id
+        const addressInsertQuery = `INSERT INTO addresses (city, country, street, pincode, user_id) VALUES($1, $2, $3, $4, $5)`;
+        await pgClient.query(addressInsertQuery, [city, country, street, pincode, userId]);
 
         res.json({
             message: "You have been signed up"
