518df81968c87b71579afde29d65b19adcb1fe98
Shivang Upadhyay <shivang14071993@gmail.com>
2026-02-22 14:33:37 +0530

    Week-17 Postgress day-2

--- Week-17/src/index.ts
+++ Week-17/src/index.ts
@@ -58,6 +58,18 @@ app.get("/metadata", async (req, res) => {
         address: response2.rows[0]
     });
 });
+
+app.get('/better-metadata', async(req, res)=>{
+    const id = req.query.id;
+
+    const query = `SELECT users.id, users.username, users.email, addresses.city, addresses.country, addresses.street, addresses.pincode FROM users JOIN addresses ON users.id = addresses.user_id WHERE users.id = $1;`
+
+    const response = await pgClient.query(query, [id]);
+    res.json({
+        response: response.rows
+    })
+
+})
 app.listen(3000);
 
 
