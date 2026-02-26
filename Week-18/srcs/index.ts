e0a0d72dcee84cd3f7dc5af238bc4ec905bb5e94
Shivang Upadhyay <shivang14071993@gmail.com>
2026-02-26 13:17:47 +0530

    Week-18

--- Week-18/srcs/index.ts
+++ Week-18/srcs/index.ts
@@ -1,5 +1,37 @@
+import express from 'express'
 import { PrismaClient } from '@prisma/client'
+
+
+const app = express()
 const client = new PrismaClient()
+
+
+app.get('/users', async(req, res)=>{
+  const users =  await client.user.findMany();
+
+  res.json({
+    users
+  })
+})
+
+app.get('/todos/:id', async(req, res)=>{
+  const id  = parseInt(req.params.id);
+  const users = await client.user.findFirst({
+    where: {
+      id : id
+    }, 
+    select: {
+      todos: true,
+      username : true
+    }
+  });
+
+  res.json({
+    users
+  })
+})
+
+app.listen(3000);
 
 // async function createUser() {
 //   await client.user.create({
