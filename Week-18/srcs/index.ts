44f2234f441a8de3286a603dc8dccd52955ec698
Shivang Upadhyay <shivang14071993@gmail.com>
2026-02-26 10:30:33 +0530

    Week-18

--- Week-18/srcs/index.ts
+++ Week-18/srcs/index.ts
@@ -1,18 +1,57 @@
 import { PrismaClient } from '@prisma/client'
 const client = new PrismaClient()
 
-async function createUser() {
-  await client.user.create({
-    data: {
-      username: "Shivang",
-      password: "123322",
-      firstName: "Shivanah",
-      lastName: "Upadhyay"
+// async function createUser() {
+//   await client.user.create({
+//     data: {
+//       username: "Shivang",
+//       password: "123322",
+//       firstName: "Shivanah",
+//       lastName: "Upadhyay"
+//     }
+//   })
+// }
+// async function deleteUser() {
+//   await client.user.delete({
+//     where:{
+//       id: 1
+//     }
+//   })
+// }
+// async function updateUser() {
+//   await client.user.update({
+//     where:{
+//       id: 1
+//     },
+//     data: {
+//       username: "Ayuuuu",
+//       password : "ayususu",
+//       firstName: "yumyum",
+//       lastName: "upadhyay"
+//     }
+//   })
+// }
+
+async function findUser(){
+  const user = await client.user.findFirst({
+    where:{
+      id : 1,
     }
   })
+
+
+  console.log(user);
 }
 
-createUser();
+findUser();
+
+
+// createUser();
+
+// deleteUser();
+// updateUser();
+
+
 
 
 
