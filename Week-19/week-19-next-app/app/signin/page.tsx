e911928f3676d6c4ad1e7cf0102494f488619ed7
Shivang Upadhyay <shivang14071993@gmail.com>
2026-03-15 23:36:56 +0530

    Some major changes

--- Week-19/week-19-next-app/app/signin/page.tsx
+++ Week-19/week-19-next-app/app/signin/page.tsx
@@ -1,6 +1,7 @@
+"use client"
 export  default function SignIn(){
-    return <div>
-        <div>
+    return <div className="w-screen h-screen flex justify-center items-center">
+        <div className="border p-2">
             <input type="text" placeholder="username"></input>
             <input type="password" placeholder="password"></input>
             <button onClick={()=>{
---
+++ Week-19/week-19-next-app/app/signup/page.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import axios from "axios"
+import { useState } from "react"
+
+export  default function SignUp(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    return <div className="w-screen h-screen flex justify-center items-center">
+        <div className="border p-2">
+            <input type="text" placeholder="username" onChange={e =>{
+                setUsername(e.target.value);
+            }}></input>
+            <input type="password" placeholder="password" onChange={e =>{
+                setPassword(e.target.value);
+            }}></input>
+            <button onClick={()=>{
+                console.log(username)
+                console.log(password)
+                axios.post("http://localhost:3000/api/v1/user/signup",{
+                    body : {
+                        username,
+                        password
+                    }
+                })
+            }}>Sign Up</button>
+        </div>
+    </div>
+}
