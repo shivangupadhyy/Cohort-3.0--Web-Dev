24852d8027a3877ccfb8b1bd076ac87060853de5
Shivang Upadhyay <shivang14071993@gmail.com>
2026-06-12 21:27:04 +0530

    CHANEGS

---
+++ week-20.2/app/api/signin/route.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken"
+import { NextRequest, NextResponse } from "next/server";
+
+export async function  POST(req: NextRequest){
+    // ideally we should check the username and password in the DB and only if it is right
+    //we Should return the jwt
+
+    const body = await req.json();
+
+    const username = body.username;
+    const password = body.password;
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, "SECRET")
+
+    return NextResponse.json({
+        token
+    })
+
+}
--- week-20/app/layout.tsx
+++ week-20/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
         <div>hi there</div>
         {children}
         <div>
-          bye
+          bye 
         </div>
         </body>
     </html>
