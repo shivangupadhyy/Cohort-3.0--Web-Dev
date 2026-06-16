26511c029bb3960a7e59cd8906dcab5ded14cd6c
Shivang Upadhyay <shivang14071993@gmail.com>
2026-06-16 12:19:02 +0530

    changes

---
+++ week-21-turborepo/apps/web/app/chat/[roomId]/page.tsx
@@ -0,0 +1,21 @@
+import { TextInput } from "@repo/ui/text-input";
+
+export default function(){
+    return <div style
+    ={{
+        width:"100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent:"space-between",
+        flexDirection :"column"
+    }}>
+
+
+        <div>
+            Chat room
+            </div>
+            <div>
+                <TextInput size="big" placeholder="Chat here"></TextInput>
+            </div>
+    </div>
+}
---
+++ week-21-turborepo/packages/ui/src/text-input.tsx
@@ -0,0 +1,18 @@
+interface ProType{
+    placeholder : string;
+    size: "big" | "small",
+}
+
+export function TextInput({
+    placeholder,
+    size,
+
+}: ProType){
+
+    return <input placeholder={placeholder} style={{
+        padding: size==="big" ? 20 : 10,
+        margin:  size==="big" ? 20 : 10,
+        borderColor : "black",
+        borderWidth : 1,
+    }}></input>
+}
